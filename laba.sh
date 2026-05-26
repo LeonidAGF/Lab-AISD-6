c412ed54963596f009ca6e692003b7206da07160
Leonid Gurianov <leonid.11521@gmail.com>
2026-05-26 10:26:14 +0300

    add bash

--- laba.sh
+++ laba.sh
@@ -9,10 +9,11 @@ read -p "col: " shift_val
 upper_shifted="${upper:$shift_val}${upper:0:$shift_val}"
 lower_shifted="${lower:$shift_val}${lower:0:$shift_val}"
 
-find "$target_dir" -type f -name "*.txt" | while read -r file; do
+find "$target_dir" -type f -name "*.txt" | while read -r file
+do
 
-    tr "$upper$lower" "$upper_shifted$lower_shifted" < "$file" > "$file.tmp"
-    mv "$file.tmp" "$file"
+    content=$(tr "$upper$lower" "$upper_shifted$lower_shifted" < "$file")
+    echo "$content" > "$file"
 
 done
 
