96509f9d0d2fd302477ab6da3905c1fc41002b9f
Leonid Gurianov <leonid.11521@gmail.com>
2026-05-26 12:30:40 +0300

    readme

--- laba.sh
+++ laba.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
 upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower="abcdefghijklmnopqrstuvwxyz"
-
 read -p "path: " target_dir
-read -p "col: " shift_val
-
-upper_shifted="${upper:$shift_val}${upper:0:$shift_val}"
-lower_shifted="${lower:$shift_val}${lower:0:$shift_val}"
-
+read -p "col: " shift
+upper_shifted="${upper:$shift}${upper:0:$shift}"
+lower_shifted="${lower:$shift}${lower:0:$shift}"
 find "$target_dir" -type f -name "*.txt" | while read -r file
 do
-
     content=$(tr "$upper$lower" "$upper_shifted$lower_shifted" < "$file")
     echo "$content" > "$file"
-
 done
-
 echo "done"
